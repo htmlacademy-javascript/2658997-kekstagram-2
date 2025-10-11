c0396f014c24803b403f333b5f83e5e090805629
Ramazan <ramazan.tsucharov@icloud.com>
2025-10-11 11:24:36 +0300

    Нужно больше функций

---
+++ js/functions.js
@@ -0,0 +1,19 @@
+function checkStringLenth (string, maxLength) {
+  return string.length <= maxLength;
+}
+
+const result = checkStringLenth('qwerty, 10');
+console.log(result);
+
+
+function isPalindrom(string) {
+  let newString = replaceAll(string);
+  let testString = '';
+  newString = toLowerCase(newString);
+
+  for (let i = length(newString) - 1; i >= 0; i--) {
+    testString += newString[i];
+  }
+
+  return newString === testString;
+}
