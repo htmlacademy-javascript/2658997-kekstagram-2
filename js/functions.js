91d985022872bda31e57a6ce86593538d0798d6a
Ramazan <ramazan.tsucharov@icloud.com>
2025-10-13 13:17:55 +0300

    Правки по заданию - Нужно больше функций

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,37 @@
-function checkStringLenth (string, maxLength) {
-  return string.length <= maxLength;
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
+console.log(checkStringLength('qwerty', 10));
+
+
+function isPalindrome(myString) {
+  myString = myString.replaceAll(' ', '').toLowerCase();
+  let result = true;
+  for (let i = 0; i < Math.floor(myString.length / 2); i++) {
+
+    if (myString[i] !== myString[myString.length - 1 - i]) {
+      result = false;
+      break;
+    }
+  }
+  return result;
 }
-
-const result = checkStringLenth('qwerty, 10');
-console.log(result);
+console.log(isPalindrome('ша шалаш аш'));
 
 
-function isPalindrom(string) {
-  let newString = string.replaceAll(' ', '');
-  let testString = '';
-  newString = string.toLowerCase();
+function getNumber (myString) {
+  myString = myString.toString(); // На случай если введено число
+  let tempString = '';
 
-  for (let i = length(newString) - 1; i >= 0; i--) {
-    testString += newString[i];
+  for (let i = 0; i < myString.length; i++) {
+    if(!isNaN(parseInt(myString[i], 10))) {
+      tempString += myString[i];
+    } else {
+      continue;
+    }
   }
 
-  return newString === testString;
+  return tempString === '' ? NaN : parseInt(tempString, 10);
 }
+
+console.log(getNumber('hello123 world 345'), typeof(getNumber('hello123 world 456')));
+console.log(getNumber('qwerty qw er ty'), typeof(getNumber('qwerty qw er ty')));
+
